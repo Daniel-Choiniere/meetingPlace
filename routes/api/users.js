b02fb0022fc5d0fd9de55abb83a7abf6a9d8a4cb
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-07-22 18:14:39 -0400

    express validator handling the error response, tested and working

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 // package that will do validation checks on submitted data
-const { check, validationResult } = require('express-validator/check');
+const { check, validationResult } = require('express-validator');
 
 // @route        POST api/users
 // description   Register User
@@ -13,7 +13,11 @@ router.post('/', [
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'please enter a password with six or more characters').isLength({ min: 6 })
 ], (req, res) => { 
-    console.log(req.body);
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     res.send('User route');
 });
 
