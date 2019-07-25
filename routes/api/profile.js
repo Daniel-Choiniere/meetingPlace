65e290dade6059894f212ac6c88d290819a212be
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-07-25 19:05:04 -0400

    working on GETing the current users profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route        GET api/profile
-// description   Test route
-// @access       Public
-router.get('/', (req, res) => res.send('Profile route'));
-
+// @route        GET api/profile/me
+// description   Get current users profile      
+// @access       Private
+router.get('/me', auth, async (req, res) => {
+    try {
+        const profile = await profile.findOne({ user: req.user.id}).populate(
+            'user',
+            ['name', 'avatar']
+        );
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router; 
