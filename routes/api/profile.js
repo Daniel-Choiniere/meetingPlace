08037e2be01b7b455967d490f137ff7556dc1673
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-07-28 12:09:44 -0400

    solved the bug, had to change the user schema ref value from user to users

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -98,14 +98,30 @@ router.post(
 // @access       Public
 router.get("/", async (req, res) => {
   try {
-    // console.log("Made it here")
-    const profiles = await Profile.find().populate("user", ["name", "avatar"]);0
-    // const profiles = ['why', 'wont', 'this', 'work'];
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
     res.json(profiles);
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
   }
 });
+
+// @route        GET api/profile/user/:user_id
+// description   Get profile by user ID
+// @access       Public
+router.get("/user/:user_id", async (req, res) => {
+    try {
+      const profile = await Profile.findOne({ user: req.params.user_id }).populate("user", ["name", "avatar"]);
+    
+      if (!profile) return res.status(400).json({ msg: "Profile not found" });  
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      if (err.kind == 'ObjectId') {
+        return res.status(400).json({ msg: 'Profile not found' });
+      }
+      res.status(500).send("Server Error");
+    }
+  });
 
 module.exports = router;
