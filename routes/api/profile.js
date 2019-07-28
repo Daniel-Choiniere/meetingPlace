bc23803591c40cc38a7f3b2046ea448d1ce85907
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-07-28 14:54:46 -0400

    sucessfully adding a user profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -142,4 +142,62 @@ router.delete("/", auth, async (req, res) => {
   });
 
 
+// @route        PUT api/profile/experience
+// description   Add profile experience
+// @access       Private
+router.put('/experience', [ auth, [
+    check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+    check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+    check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+ ], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // pulls this data out of req.body and defines it to variables we can work with
+        const {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description 
+        } = req.body;
+
+        // create a new object
+        const newExp = {
+            title,
+            company,
+            location,
+            from,
+            to,
+            current,
+            description 
+        };
+
+        try {
+            const profile = await Profile.findOne({ user: req.user.id });
+
+            // pushes the newExp object we created into the begining of the profile array
+            profile.experience.unshift(newExp);
+          
+            await profile.save();
+
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+});
+
 module.exports = router;
