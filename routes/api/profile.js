0f302b4c92db6cbe5f5a2be5d8ea676807f59c82
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-07-30 19:49:37 -0400

    succesfully GETing a github user profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
@@ -207,7 +209,6 @@ router.delete('/experience/:exp_id', auth, async (req, res) => {
   try {
     const foundProfile = await Profile.findOne({ user: req.user.id });
     const expIds = foundProfile.experience.map(exp => exp._id.toString());
-    // if i dont add .toString() it returns this weird mongoose coreArray and the ids are somehow objects and it still deletes anyway even if you put /experience/5
     const removeIndex = expIds.indexOf(req.params.exp_id);
     if (removeIndex === -1) {
       return res.status(500).json({ msg: "Server error" });
@@ -294,7 +295,6 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   try {
     const profile = await Profile.findOne({ user: req.user.id });
     const eduIds = profile.education.map(edu => edu._id.toString());
-    // if i dont add .toString() it returns this weird mongoose coreArray and the ids are somehow objects and it still deletes anyway even if you put /experience/5
     const removeIndex = eduIds.indexOf(req.params.edu_id);
     if (removeIndex === -1) {
       return res.status(500).json({ msg: "Server error" });
@@ -310,4 +310,39 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
   }
 });
 
+// @route        Get api/profile/github/:username
+// description   Get user repos from Github
+// @access       Public
+router.get('/github/:username', (req, res) => {
+  try {
+      const options = {
+        uri: `https://api.github.com/users/${
+          req.params.username
+        }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+          'githubClientId'
+        )}&client_secret=${config.get('githubSecret')}`,
+        method: 'GET',
+        headers: { 'user-agent': 'node.js' }
+      };
+
+      request(options, (error, response, body) => {
+        if (error) {
+          console.error(error);
+        } 
+
+        if (response.statusCode !== 200) {
+          return res.status(404).json({ msg: "No github profile found " });
+        } 
+        
+      res.json(JSON.parse(body));
+      });
+    
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ msg: "Server error" })
+  }
+})
+
+
 module.exports = router;
+
