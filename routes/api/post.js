035b9f89534f68ed7a77ccddcba906db3f9054a8
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-07-30 23:42:43 -0400

    working on create a popst function

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 
-// @route        GET api/posts
-// description   Test route
-// @access       Public
+// @route        POST api/posts
+// description   Create a post
+// @access       Private
 router.get('/', (req, res) => res.send('Post route'));
 
 module.exports = router; 
