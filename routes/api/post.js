671744d527a2354f05570e1e276096e2c0353216
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-07-31 22:54:25 -0400

    get all posts route working

--- routes/api/post.js
+++ routes/api/post.js
@@ -46,4 +46,18 @@ async (req, res) => {
     }
 });
 
+// @route        GET api/posts
+// description   Get  all posts
+// @access       Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+        res.json(posts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
 module.exports = router; 
