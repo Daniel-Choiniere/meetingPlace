bcec0acc172016aeaedd7161b69897ec947ff1f1
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-08 06:33:17 -0400

    functiionality to make a comment on a post working

--- routes/api/post.js
+++ routes/api/post.js
@@ -154,4 +154,58 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 })
 
+// @route        POST api/posts/comment/:id
+// description   Comment on a post
+// @access       Private
+router.post('/comment/:id', 
+[
+    auth, 
+    [
+    check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+    ], 
+async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+          // gets the user, name, and avatar
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+        
+        const newComment =  ({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+        
+        res.json(post.comments);
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route        DELETE api/posts/comment/:id/:commnet_id
+// description   Delete a comment
+// @access       Private
+router.delete('api/posts/comment/:id/:commnet_id', auth, async (req, res) => {
+    try{
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    } 
+});
+
 module.exports = router; 
