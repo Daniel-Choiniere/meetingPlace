d13a8ecd57cb5f9c1a54e3c17df1b3b7c3d5a01e
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-10 23:23:32 -0400

    have redux store built and setup

--- client/src/store.js
+++ client/src/store.js
@@ -2,3 +2,15 @@ import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
+
+const intitialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  intitialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
