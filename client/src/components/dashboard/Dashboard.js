764f85ef20a85b55baa833cbfe8df05cdacfdb1e
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-14 22:05:49 -0400

    nuilding dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 const Dashboard = props => {
   return <div>Dashboard</div>;
@@ -7,4 +8,7 @@ const Dashboard = props => {
 
 Dashboard.propTypes = {};
 
-export default Dashboard;
+export default connect(
+  mapStateToProps,
+  { getCurrentProfile }
+)(Dashboard);
