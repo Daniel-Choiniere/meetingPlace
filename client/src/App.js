15f554884dadb9ed4ea7b1a1b61ad6e34b146f3d
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-18 20:03:06 -0400

    able to access the profile add an expereince form

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import NotFound from "./components/layout/NotFound";
+import AddExperience from "./components/profile-forms/AddExperience";
 import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
@@ -47,6 +48,11 @@ const App = () => {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
               />
               <Route component={NotFound} />
             </Switch>
