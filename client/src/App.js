3d493f7f0f610aff213b596845624f732e96610d
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-18 20:33:01 -0400

    user able too education experience to their profile. Saving to mongoDB database

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import NotFound from "./components/layout/NotFound";
 import AddExperience from "./components/profile-forms/AddExperience";
+import AddEducation from "./components/profile-forms/AddEducation";
 import PrivateRoute from "./components/routing/PrivateRoute";
 // Redux
 import { Provider } from "react-redux";
@@ -53,6 +54,11 @@ const App = () => {
                 exact
                 path="/add-experience"
                 component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
               />
               <Route component={NotFound} />
             </Switch>
