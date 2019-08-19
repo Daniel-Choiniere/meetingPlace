35b16a148486c7ef472a01fe2eac289d41e9fe97
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-18 23:36:42 -0400

    commitign changes to work remote tommorow

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from "./types";
+import {
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+  CLEAR_PROFILE,
+  ACCOUNT_DELETED
+} from "./types";
 
 // Get current users profile
 export const getCurrentProfile = () => async dispatch => {
@@ -115,5 +121,62 @@ export const addEducation = (formData, history) => async dispatch => {
       type: PROFILE_ERROR,
       payload: { msg: err.message.statusText, status: err.response.status }
     });
+  }
+};
+
+// Delete an experience
+export const deleteExperience = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Experience removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.message.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete an education
+export const deleteEducation = id => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/profile/education/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Education removed", "success"));
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.message.statusText, status: err.response.status }
+    });
+  }
+};
+
+// Delete a profile
+export const deleteAccount = id => async dispatch => {
+  if (window.confirm("Are you sure? This can NOT be undone!")) {
+    try {
+      const res = await axios.delete(`/api/profile`);
+
+      dispatch({ type: CLEAR_PROFILE });
+      dispatch({ type: ACCOUNT_DELETED });
+
+      dispatch(setAlert("your account has been permanantly deleted"));
+    } catch (err) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: { msg: err.message.statusText, status: err.response.status }
+      });
+    }
   }
 };
--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -2,7 +2,7 @@ import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 import Moment from "react-moment";
 import moment from "moment";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 
 const Education = ({ education }) => {
   const educations = education.map(edu => (
