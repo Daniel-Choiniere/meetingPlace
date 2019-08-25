2c465ca9f80e0acc760d79f711ed282032f9df8e
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-25 17:23:21 -0400

    have each prfile being displayed in a list on the developer page

--- client/src/components/profiles/Profileitem.js
+++ client/src/components/profiles/Profileitem.js
@@ -23,6 +23,13 @@ const Profileitem = ({
           View Profile
         </Link>
       </div>
+      <ul>
+        {skills.slice(0, 4).map((skill, index) => (
+          <li key={index} className="text-primary">
+            <i className="fas fa-check" /> {skill}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
