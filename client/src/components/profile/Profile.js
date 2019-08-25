ea5e4440dfbc9dec36b15fda9e37a2fecd12be65
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-25 18:18:01 -0400

    implemented a back to all profiles button , and an edit the profile if the current logged in user

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,20 +1,40 @@
 import React, { Fragment, useEffect } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import Spinner from "../layout/spinner";
 import { getProfileById } from "../../actions/profile";
 
 const Profile = ({
   getProfileById,
-  profiel: { profile, loading },
+  profile: { profile, loading },
   auth,
   match
 }) => {
   useEffect(() => {
     getProfileById(match.params.id);
-  }, [getProfileById, match]);
+  }, [getProfileById, match.params.id]);
 
-  return <div>profile</div>;
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to="/profiles" className="btn btn-light">
+            Back to Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to="/edit-profile" className="btn btn-dark">
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profile.propTypes = {
