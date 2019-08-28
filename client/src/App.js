ca97daa4d49f75ec371a348cd56eee2f5bd51f79
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-27 22:43:17 -0400

    have our post route, succesfully fetching posts

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import CreateProfile from "./components/profile-forms/CreateProfile";
 import EditProfile from "./components/profile-forms/EditProfile";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
+import Posts from "./components/posts/Posts";
 import NotFound from "./components/layout/NotFound";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
@@ -64,6 +65,7 @@ const App = () => {
                 path="/add-education"
                 component={AddEducation}
               />
+              <PrivateRoute exact path="/posts" component={Posts} />
               <Route component={NotFound} />
             </Switch>
           </section>
