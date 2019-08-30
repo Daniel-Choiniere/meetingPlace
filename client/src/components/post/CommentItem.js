7c4762c14beafdafe1d2949d100a90a0525ae729
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2019-08-29 20:09:36 -0400

    have app fully functional. All routes are working. We can add and modify  users, profiles, comments, posts

--- client/src/components/post/CommentItem.js
+++ client/src/components/post/CommentItem.js
@@ -3,11 +3,13 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import Moment from 'react-moment';
+import { deleteComment } from '../../actions/post'
 
 const CommentItem = ({
   postId,
   comment: { _id, text, name, avatar, user, date },
-  auth
+  auth,
+  deleteComment
 }) => 
   <div className="post bg-white p-1 my-1">
     <div>
@@ -27,6 +29,11 @@ const CommentItem = ({
        <p className="post-date">
           Posted on <Moment format='YYYY/MM/DD'>{date}</Moment>
       </p>
+      {!auth.loading && user === auth.user._id && (
+          <button onClick={e => deleteComment(postId, _id)} type="button" className="btn btn-danger">
+              <i className="fas fa-times"></i>
+          </button>
+      )}
     </div>
   </div>
 
@@ -34,7 +41,8 @@ const CommentItem = ({
 CommentItem.propTypes = {
   postId: PropTypes.number.isRequired,
   comment: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
+  deleteComment: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
@@ -43,5 +51,5 @@ const mapStateToProps = state => ({
 
 export default connect(
   mapStateToProps,
-  {}
+  {deleteComment}
 )(CommentItem);
